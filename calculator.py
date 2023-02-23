b8b84e89e73efc86e6204dde7bacad71c16b26c0
Alvaro Alvarez <aalvarezss@alumnos.unex.es>
2023-02-23 19:01:19 +0100

    Taggear

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# Custom function to read the two numbers.
+#e Custom function to read the two numbers.
 def read_numbers():
     num1 = float(input('Enter first number: '))
     num2 = float(input('Enter second number: '))
